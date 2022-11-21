893e08fe50112720effd8e0dc6e7a4c6b895de7e
Jose Alfredo Valor <jvalor.externo@coopeuch.cl>
2022-11-21 06:40:11 -0500

    Add animations and floating categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories as mockCategories } from '../../../api/db.json'
+import { categories } from '../../../api/db.json'
 
-export const ListOfCategories = () => (
-  
-  return(
-    <List>
-    {categories.map(category => <Item key={category.id}><Category {...category} /></Item>)}
-  </List>
+export const ListOfCategories = () => {
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
+      {categories.map(category => <Item key={category.id}><Category {...category} /></Item>)}
+    </List>
   )
-)
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
+  )
+}
